942e55b88ea69b1bf9010447620594a2c24c4be7
Francisco Fernandez <ffernandezcasas@gmail.com>
2018-05-26 15:46:43 -0400

    ProdCientifica start

---
+++ ProdCientifica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProdCientifica.Startup))]
+namespace ProdCientifica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
